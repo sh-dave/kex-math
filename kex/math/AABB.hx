b14770eca55a28f37eaaac17c9eb4f56ba0c54f1
David Klein <david.klein@psmtec.de>
2018-10-29 10:51:55 +0100

    better variable names

--- kex/math/AABB.hx
+++ kex/math/AABB.hx
@@ -2,53 +2,39 @@ package kex.math;
 
 // TODO (DK) add a FastAABB using kha.FastFloat?
 class AABB {
-	// min
-	public var mx: Float;
-	public var my: Float;
-	public var mz: Float;
-	// max
-	public var xx: Float;
-	public var xy: Float;
-	public var xz: Float;
-
-	//public static function createEmpty()
-		//return new AABB(1.0, 0.0, 0.0, -1.0, 0.0, 0.0);
+	public var minX: Float;
+	public var minY: Float;
+	public var minZ: Float;
+	public var maxX: Float;
+	public var maxY: Float;
+	public var maxZ: Float;
 
 	public static function createEmpty()
 		return new AABB(0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 
-	@:external inline function new( mx, my, mz, xx, xy, xz ) {
-		this.mx = mx; this.my = my; this.mz = mz;
-		this.xx = xx; this.xy = xy; this.xz = xz;
+	@:extern inline function new(
+		minX: Float, minY: Float, minZ: Float,
+		maxX: Float, maxY: Float, maxZ: Float
+	) {
+		this.minX = minX; this.minY = minY; this.minZ = minZ;
+		this.maxX = maxX; this.maxY = maxY; this.maxZ = maxZ;
 	}
 
-	@:external public inline function setFromOther( other: AABB ) {
-		mx = other.mx; my = other.my; mz = other.mx;
-		xx = other.xx; xy = other.xy; xz = other.xz;
+	@:extern public inline function setFromOther( other: AABB ) : AABB {
+		minX = other.minX; minY = other.minY; minZ = other.minZ;
+		maxX = other.maxX; maxY = other.maxY; maxZ = other.maxZ;
+		return this;
 	}
 
-	@:external public inline function setNull() {
-		//mx = 1.0; my = 0.0; mz = 0.0;
-		//xx = -1.0; xy = 0.0; xz = 0.0;
-
-		mx = 0.0; my = 0.0; mz = 0.0;
-		xx = 0.0; xy = 0.0; xz = 0.0;
+	@:extern public inline function setNull() : AABB {
+		minX = 0.0; minY = 0.0; minZ = 0.0;
+		maxX = 0.0; maxY = 0.0; maxZ = 0.0;
+		return this;
 	}
 
-	//public inline function isNull() : Bool
-		//return mx > xx || my > xy || mz > xz;
-
-	// TODO (DK) the 'new AABB' doesn't seem to get inlined
-	@:external public inline function extendVectorComponents( x: Float, y: Float, z: Float ) : AABB
+	@:extern public inline function extendVectorComponents( x: Float, y: Float, z: Float ) : AABB
 		return new AABB(
-			Math.min(mx, x), Math.min(my, y), Math.min(mz, z),
-			Math.max(xx, x), Math.max(xy, y), Math.max(xz, z)
+			Math.min(minX, x), Math.min(minY, y), Math.min(minZ, z),
+			Math.max(maxX, x), Math.max(maxY, y), Math.max(maxZ, z)
 		);
-
-		//return isNull()
-			//? new AABB(x, y, z, x, y, z)
-			//: new AABB(
-				//Math.min(mx, x), Math.min(my, y), Math.min(mz, z),
-				//Math.max(xx, x), Math.max(xy, y), Math.max(xz, z)
-			//);
 }
